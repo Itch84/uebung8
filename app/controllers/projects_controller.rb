23f926defff08a360b560a06773c95f79319da45
Itch84 <itch84@gmail.com>
2013-12-17 15:20:32 +0100

    L??schen von Projekten

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,7 +17,13 @@ class ProjectsController < ApplicationController
     end
   end
 
-
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+  end
+  
+  
+  
 private
 
     def project_params
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -39,5 +39,14 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_response :success
   end
   
+  test "should destroy project" do
+    sign_in @user
+    assert_difference('Project.count', -1) do
+      xhr :delete, :destroy, id: @project
+    end
+    assert_response :success
+  end
+  
+  
   
 end
