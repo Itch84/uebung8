547cfdae8550ea7be02c8122de2c30ee18e61c5a
Itch84 <itch84@gmail.com>
2013-12-17 15:40:45 +0100

    Task kann Projekt zugeordnet werden + Task-Index mit Projekt

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -81,6 +81,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :deadline, :done, :duration, :delegated_id)
+      params.require(:task).permit(:name, :deadline, :done, :duration, :delegated_id, :project_id)
     end
 end
